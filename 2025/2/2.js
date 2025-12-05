37bb688c43465f18dc113a1c20cca27ab5ebbab1
wicke <wicke0719@gmail.com>
2025-12-04 21:24:23 -0500

    Day 2 Part 1

--- 2025/2/2.js
+++ 2025/2/2.js
@@ -1,14 +1,35 @@
 const fs = require("fs");
-var text = fs.readFileSync("data.txt", "utf8");
+var text = fs.readFileSync("data1.txt", "utf8");
 var data = text.split(",");
 
-console.log(data);
+matchCount = 0;
+matchTotal = 0;
 
-data.map((id) => {
-  range = id.split("-");
-  firstLength = range[0].length;
-  console.log(firstLength);
-  for (var x = range[0]; x <= range[1]; x++) {
-    console.log("CHECKING: " + x);
+const idCheck = (itemStr) => {
+  const itemLen = itemStr.length;
+  if (itemLen % 2 == 0) {
+    const splitPt = itemLen / 2;
+    const stringHalves = [itemStr.slice(0, splitPt), itemStr.slice(splitPt)];
+    const [firstHalf, secondHalf] = stringHalves.map((s) => parseInt(s, 10));
+    return firstHalf === secondHalf;
   }
-});
+  return false;
+};
+
+for (const dataRange of data) {
+  const [startStr, endStr] = dataRange.split("-");
+  const start = parseInt(startStr, 10);
+  const end = parseInt(endStr, 10);
+  for (let i = start; i <= end; i++) {
+    const iStr = i.toString();
+    const check = idCheck(iStr);
+    if (check) {
+      matchTotal += i;
+      matchCount++;
+      // console.log("MATCH");
+    }
+  }
+}
+
+console.log(`TOTAL MATCHES COUNT: ${matchCount}`);
+console.log(`TOTAL MATCHES SUM: ${matchTotal}`);
